c568ebf8efd0bd01aebbb3e4a663f3e1bcb748ca
evklid009 <88339620+evklid009@users.noreply.github.com>
2021-12-06 16:49:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    $('.list-item').hover(function() {
+        $(this).toggleClass('active');
+    });
+    
+    $('.list-item-2').hover(function() {
+        $(this).toggleClass('active');
+    });
+
+    $('.list-item:eq(0)').on('click', function() {
+        $('.img-one').fadeToggle('slow');
+    });
+
+    $('.list-item:eq(1)').on('click', function() {
+        $('.img-two').fadeToggle('slow');
+    });
+
+    $('.list-item:eq(2)').on('click', function() {
+        $('.img-three').fadeToggle('slow');
+    });
+
+    $('.list-item:eq(3)').on('click', function() {
+        $('.img-four').fadeToggle('slow');
+    });
+
+    $('.list-item:eq(4)').on('click', function() {
+        $('.img-five').animate({
+            opacity: 'toggle',
+            height: 'toggle',
+            width: 'toggle'
+        },2000);
+    });
+
+    $('.list-item:eq(5)').on('click', function() {  
+        $('.img-six').animate({
+            width: [ "toggle", "swing" ],
+            height: [ "toggle", "swing" ],
+            opacity: "toggle" 
+        }, 1500, "linear");
+
+    });
+
+    $('.list-item-2').on('click', function() {
+        setTimeout(function(){
+            location.reload(true);            
+        },1000);  
+    });
+
+    $("#popup").css("display","block");
+    $(".popup-close").click(function(){
+    $("#popup").fadeOut(700);
+    });
+
+});
+
+function clock() {
+    var d = new Date();
+    var day = d.getDate();
+    var hrs = d.getHours();
+    var min = d.getMinutes();
+    var sec = d.getSeconds();
+    
+    var mnt = new Array(01, 02, 03, 04, 05,
+    06, 07, 08, 09, 10, 11, 12);
+    
+    if (day <= 9) day="0" + day;
+    if (hrs <= 9) hrs="0" + hrs;
+    if (min <=9 ) min="0" + min;
+    if (sec <= 9) sec="0" + sec;
+    
+    $("#time").html(hrs + ":" + min + ":" + sec + "&nbsp;&nbsp;&nbsp;" +
+    day + "." + mnt[d.getMonth()] + " " + d.getFullYear() + " г.");
+}
+setInterval("clock()",100);
+
+
